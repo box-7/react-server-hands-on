69a2b537ccc3a8f7e52c9afb5bb98b0865ed32ae
box-7 <hayainada1981@yahoo.co.jp>
2025-03-16 21:08:27 +0900

    コード解析

--- src/domain/Article.ts
+++ src/domain/Article.ts
@@ -1,4 +1,7 @@
 // Convexから取得したデータはこのクラスに変換して使う
+// constructor は、クラスのインスタンスを初期化するための特別なメソッド
+// クラスの新しいインスタンスが作成されるときに自動的に呼び出され、そのクラスのプロパティを初期化するために使用される
+
 export class Article {
         constructor(
           public id: string,
